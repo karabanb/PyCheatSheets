72a970ae910fc3cdec3d36a6b4de6bff53ff26a6
karabanb <b.karaban@me.com>
2019-03-29 20:23:33 +0100

    init commit

--- scripts/DataManipulation.py
+++ scripts/DataManipulation.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 path = '/Users/bkaraban/PycharmProjects/PyCheatSheets/data/train.csv'
@@ -9,15 +8,22 @@ titanic = pd.read_csv(path, index_col='PassengerId')
 
 first_few_rows = titanic[0:5]
 first_few_rows_loc = titanic.loc[0:5]
+first_few_rows_iloc = titanic.iloc[0:5]
+
 
 ''' selecting columns'''
 
-one_col = titanic['Pclass']     # in this way we reached pandas.Series type
-one_col = titanic[['Pclass']]   # but like that we got pandas.DataFrame type
+one_col = titanic['Pclass']                                 # in this way we reached pandas.Series type
+one_col = titanic[['Pclass']]                               # but like that we got pandas.DataFrame type
 
-one_col = titanic.loc[:, 'Pclass']
-one_col = titanic.loc[:, ['Pclass']]
+# slicing columns
 
-one_col = titanic.iloc[:, 1]
+one_col = titanic.loc[:, 'Pclass']                          # pandas.Series
+one_col = titanic.loc[:, ['Pclass']]                        # pandas.Dataframe
+
+one_col = titanic.iloc[:, 1]                                # pandas.Series
 
 few_cols = titanic[['Pclass', 'Sex', 'Survived']]
+few_cols = titanic.loc[:, ['Pclass', 'Sex', 'Survived']]
+
+few_cols_iloc = titanic.iloc[:, 0:4]
