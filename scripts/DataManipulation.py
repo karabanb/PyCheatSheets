61af273e3dace349c8e914be67c970bf5b7d757d
karabanb <b.karaban@me.com>
2019-03-30 10:58:48 +0100

    adding examples with dealing wih NA's

--- scripts/DataManipulation.py
+++ scripts/DataManipulation.py
@@ -8,15 +8,15 @@ titanic = pd.read_csv(path, index_col='PassengerId')
 
 # single column
 
-one_col = titanic['Pclass']                                 # in this way we reached pandas.Series type
-one_col = titanic[['Pclass']]                               # but like that we got pandas.DataFrame type
+one_col = titanic['Pclass']                                     # in this way we reached pandas.Series type
+one_col = titanic[['Pclass']]                                   # but like that we got pandas.DataFrame type
 
 # slicing columns
 
 one_col_loc = titanic.loc[:, 'Pclass']                          # pandas.Series
-one_col_loc = titanic.loc[:, ['Pclass']]                        # pandas.Dataframe
+one_col_loc = titanic.loc[:, ['Pclass']]                        # pandas.DataFrame
 
-one_col_iloc = titanic.iloc[:, 1]                                # pandas.Series
+one_col_iloc = titanic.iloc[:, 1]                               # pandas.Series
 
 few_cols = titanic[['Pclass', 'Sex', 'Survived']]
 few_cols_loc = titanic.loc[:, ['Pclass', 'Sex', 'Survived']]
@@ -40,4 +40,16 @@ first_few_rows_iloc = titanic.iloc[0:5]
 
 ############################################# FILTERING ROWS ###########################################################
 
+# filtering by boolean array
 
+survived = titanic['Survived'] == 1
+survived = titanic.loc[survived, :]
+
+
+############################################# FILTERING NA's ###########################################################
+
+rows_at_least_one_na = titanic.dropna(how='any')            # drop rows with at least one NA
+rows_all_nas = titanic.dropna(how='all')                    # drop rows with all NA's
+
+cols_at_least_one_na = titanic.dropna(axis=1, how='any')    # drop cols with at least one NA
+cols_all_nas = titanic.dropna(axis=1, how='all')            # drop cols with all NA's
