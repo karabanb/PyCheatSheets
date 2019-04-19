85787d3a10611fb5dc2f05a88f83db05b6182fd6
karabanb <b.karaban@me.com>
2019-04-19 20:03:04 +0200

    minor improvements

--- scripts/df_summary.py
+++ scripts/df_summary.py
@@ -1,32 +1,28 @@
 
-
-
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('/Users/bkaraban/PycharmProjects/PyCheatSheets/data/train.csv', index_col='PassengerId')
 
-result = pd.DataFrame()
-# def df_summary(df):
-types = df.dtypes
-types.name = 'type'
-
-n_obs = len(df)
-n_nas = np.sum(df.isna()).rename('n_nas')
-pct_nas = (n_nas/n_obs).rename('pct_nas')
-
-n_zeros = np.sum(df.apply(lambda x: x == 0)).rename('n_zeros')
-pct_zeros = (n_zeros/n_obs).rename('pct_zeros')
-
-n_distinct = df.nunique().rename('n_distinct')
-
-quantiles = df.describe().transpose().iloc[:, 1:]
-
-to_concat = [types, n_distinct, n_nas, pct_nas, n_zeros, pct_zeros]
-
-result = pd.concat(to_concat, axis=1).sort_values(by='type')
+# result = pd.DataFrame()
 
 
+def df_summary(df):
 
+    types = df.dtypes
+    types.name = 'type'
 
+    n_obs = len(df)
+    n_nas = np.sum(df.isna()).rename('n_nas')
+    pct_nas = (n_nas / n_obs).rename('pct_nas')
 
+    n_zeros = np.sum(df.apply(lambda x: x == 0)).rename('n_zeros')
+    pct_zeros = (n_zeros / n_obs).rename('pct_zeros')
+
+    n_distinct = df.nunique().rename('n_distinct')
+
+    quantiles = df.describe().transpose().iloc[:, 1:]
+
+    to_concat = [types, n_distinct, n_nas, pct_nas, n_zeros, pct_zeros]
+
+    result = pd.concat(to_concat, axis=1).sort_values(by='type')
